0f24ee0944d717a5a80053b434203cb2c7bb9bdc
thr828 <18550202789@126.com>
2023-06-28 14:25:00 +0800

    first

---
+++ CRC.cs
@@ -0,0 +1,65 @@
+namespace ComDe
+{
+   	#region 16位CRC校验
+    public static class CRC
+    {
+        /// <summary>
+        /// CRC校验，参数data为byte数组
+        /// </summary>
+        /// <param name="data">校验数据，字节数组</param>
+        /// <returns>字节0是高8位，字节1是低8位</returns>
+        public static byte[] CRC16(byte[] data)
+        {
+            //crc计算赋初始值
+            int crc = 0xffff;
+            for (int i = 0; i < data.Length; i++)
+            {
+                crc = crc ^ data[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    int temp;
+                    temp = crc & 1;
+                    crc = crc >> 1;
+                    crc = crc & 0x7fff;
+                    if (temp == 1)
+                    {
+                        crc = crc ^ 0xa001;
+                    }
+                    crc = crc & 0xffff;
+                }
+            }
+            //CRC寄存器的高低位进行互换
+            byte[] crc16 = new byte[2];
+            //CRC寄存器的高8位变成低8位，
+            crc16[1] = (byte)((crc >> 8) & 0xff);
+            //CRC寄存器的低8位变成高8位
+            crc16[0] = (byte)(crc & 0xff);
+            return crc16;
+        }
+    
+          public static byte[] CRC16_1(byte[] data)
+        {            
+            int len = data.Length;
+            if (len > 0)
+            {
+                ushort crc = 0xFFFF;
+
+                for (int i = 0; i < len; i++)
+                {
+                    crc = (ushort)(crc ^ (data[i]));
+                    for (int j = 0; j < 8; j++)
+                    {
+                        crc = (crc & 1) != 0 ? (ushort)((crc >> 1) ^ 0xA001) : (ushort)(crc >> 1);
+                    }
+                }
+                byte hi = (byte)((crc & 0xFF00) >> 8);  //高位置
+                byte lo = (byte)(crc & 0x00FF);         //低位置
+
+                return new byte[] { lo, hi };
+            }
+            return new byte[] { 0, 0 };
+        }
+    }
+    #endregion
+
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Ports;
+using ComDe;
+
+Console.WriteLine("Hello, World!");
+SerialPort serialPort = new SerialPort();
+
+serialPort.PortName = "COM1";
+serialPort.BaudRate = 9600;
+serialPort.DataBits = 8;
+serialPort.Parity = Parity.None;
+serialPort.StopBits = StopBits.One;
+
+serialPort.Open();
+// while(true)
+// {
+
+
+
+short startAdd = 0;
+short writeLen = 10;
+short data=-256;
+
+List<byte> bytelst = new List<byte>();
+bytelst.Add(0x01);
+//bytelst.Add(0x01);
+//bytelst.Add(0x03);
+
+// bytelst.Add(BitConverter.GetBytes(startAdd)[1]);
+// bytelst.Add(BitConverter.GetBytes(startAdd)[0]);
+
+// bytelst.Add(BitConverter.GetBytes(writeLen)[1]);
+// bytelst.Add(BitConverter.GetBytes(writeLen)[0]);
+
+#region  write single coil
+// bytelst.Add(0x05);//写单个线圈寄存器
+// bytelst.Add(0x00);
+// bytelst.Add(0x04);
+// bytelst.Add(0xFF);//写单个线圈时,写值只能为0xFF00(on),0x0000 (off),其他均为非法值
+// bytelst.Add(0x00);
+#endregion
+#region write single register
+//bytelst.Add(0x06);//写单个保持寄存器
+// bytelst.Add(0x00);
+// bytelst.Add(0x01);
+//  bytelst.Add(BitConverter.GetBytes(startAdd)[1]);
+//  bytelst.Add(BitConverter.GetBytes(startAdd)[0]);
+
+// bytelst.Add(0x0F);
+// bytelst.Add(0x10);
+//  bytelst.Add(BitConverter.GetBytes(data)[1]);
+//  bytelst.Add(BitConverter.GetBytes(data)[0]);
+#endregion 
+
+#region write many coils
+// //03 0F 00 00 00 0B 02 FF FF
+// bytelst.Add(0x0F);//写多个线圈寄存器    
+// //起始地址
+// bytelst.Add(0x00);
+// bytelst.Add(0x00);
+// //写入线圈个数
+// bytelst.Add(0x00);
+// bytelst.Add(0x0B);
+// //写入字节数
+// bytelst.Add(0x02);
+// //写入字节数
+// bytelst.Add(0xFF);
+// bytelst.Add(0xFF);
+
+
+#endregion
+//03 10 00 00 00 02 04 42 B5 33 33 A9 6C 
+bytelst.Add(0x10);//写多个保持寄存器 
+//起始地址
+bytelst.Add(0x00);
+bytelst.Add(0x02);
+//写入寄存器数量
+bytelst.Add(0x00);
+bytelst.Add(0x02);
+//写入字节数
+bytelst.Add(0x04);
+//写入字节数
+bytelst.Add(0x42);
+bytelst.Add(0xB5);
+bytelst.Add(0x33);
+bytelst.Add(0x33);
+
+#region write many registers
+
+#endregion
+
+var bytes = CRC.CRC16_1(bytelst.ToArray());
+bytelst.Add(bytes[0]);
+bytelst.Add(bytes[1]);
+
+
+//byte[] bytes = { 0x01, 0x01, 0x00, 0x00, 0x00, 0x0A, 0xBC, 0x0D };
+System.Console.WriteLine("发送:");
+foreach (var item in bytelst)
+{
+    System.Console.WriteLine(string.Format("{0:X2}", item));
+}
+while (true)
+{
+    serialPort.Write(bytelst.ToArray(), 0, bytelst.Count);
+    serialPort.DataReceived += SerialPort_DataReceived;
+    Thread.Sleep(1000);
+}
+
+
+
+void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+{
+
+    int length = serialPort.BytesToRead;
+    byte[] bytes = new byte[length];
+    serialPort.Read(bytes, 0, length);
+    // string text= Encoding.UTF8.GetString(bytes);
+    System.Console.WriteLine("接收:");
+    foreach (var item in bytes)
+    {
+        System.Console.WriteLine(string.Format("{0:X2}", item));
+    }
+
+}
+
+
